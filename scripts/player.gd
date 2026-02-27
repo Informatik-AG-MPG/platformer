03777b29b06ad60701143f99ce79161b01e773c7
michael-a-jung <152505444+michael-a-jung@users.noreply.github.com>
2026-02-27 17:27:07 +0100

    feat: basic environment, player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,35 @@
 extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
 
-
-const SPEED = 100.0
-const JUMP_VELOCITY = -200
-
+const SPEED = 150.0
+const JUMP_VELOCITY = -300
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravitation
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+	# Springen
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# Das ist dasselbe wie: Input.get_action_strength("positiv") - Input.get_action_strength("negativ")
+		animated_sprite.play("jump")
+		
+	# Wird entweder 0, 1 oder -1 sein
 	var direction := Input.get_axis("left", "right")
-	# Falls "direction" einen Wert hat, beschleunige den Spieler
-	if direction:
+	# Animationen 
+	# Sprite spiegeln
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
+	# Lauf-Animation
+	if direction == 0 && velocity.y == 0: #&& animated_sprite.animation != "jump":
+		animated_sprite.play("idle")
+	elif direction != 0 && velocity.y == 0: #&& animated_sprite.animation != "jump" && animated_sprite.is_playing() == false:
+		animated_sprite.play("walk")
+
+	if direction != 0:
 		velocity.x = direction * SPEED
-	# Falls nicht, lasse die Geschwindigkeit des Spielers langsam auf 0 fallen. (negative Beschleunigung).
+	# Spieler Geschwindigkeit linear auf 0 fallen lassen (negative Beschleunigung).
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
