5cb806fda4f4cdccf0f57019c51cdb42f22bbddc
michael-a-jung <152505444+michael-a-jung@users.noreply.github.com>
2026-06-09 22:09:27 +0200

    refactor: debug message cleanup
    also string formatting is kinda cool

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Hello")
-	print(body.name)
+	# See here for string concatonation reference: https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
+	# print("Debug - Body `%s` entered goal" % body.name)
 	if body.name == "player":
-		print("cool")
+		# switch to end scene
 		get_tree().change_scene_to_file("res://scenes/end.tscn")
