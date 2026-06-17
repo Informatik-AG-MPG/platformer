3bb067934b527c719892adfff0d175093a453623
michael-a-jung <152505444+michael-a-jung@users.noreply.github.com>
2026-06-17 21:19:48 +0200

    feat: delay when switching levels
    request #6

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+@onready var timer = $Timer
+
 func _on_body_entered(body: Node2D) -> void:
 	# See here for string concatonation reference: https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
 	# print("Debug - Body `%s` entered goal" % body.name)
 	if (body.name == "player"):
-		# switch to end scene
-		get_tree().change_scene_to_file("res://scenes/end.tscn")
+		timer.start()
+
+func _on_timer_timeout() -> void:
+	# switch to end scene
+	get_tree().change_scene_to_file("res://scenes/end.tscn")
